944290ed4a3997261bffe50cfb17a3ad4cb9f561
Dimas Ferreira <dimaslimaferreira@gmail.com>
2023-08-19 01:07:16 -0300

    aula06

--- src/main/java/academy/devdojo/springboot2/curso/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/curso/controller/AnimeController.java
@@ -1,28 +1,22 @@
 package academy.devdojo.springboot2.curso.controller;
-
 import java.time.LocalDateTime;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import academy.devdojo.springboot2.curso.domain.Anime;
 import academy.devdojo.springboot2.curso.util.DateUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController // Anotação indicando que esta classe é um controlador REST
 @RequestMapping("anime") // Mapeia a URL base para todas as rotas neste controlador
 @Log4j2 // Cria automaticamente um logger com o Lombok e Log4j2
-
+@RequiredArgsConstructor
 public class AnimeController {
 
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
 
-    // Injetando a dependência DateUtil no construtor
-    public AnimeController(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
-    }
 
     // Mapeando a URL localhost:8080/anime/list para este método
     @GetMapping(path = "list")
