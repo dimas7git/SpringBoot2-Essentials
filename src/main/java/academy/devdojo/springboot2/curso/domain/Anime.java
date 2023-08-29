b3b02819e91738c8df464201c71df0ef4ab0fdb9
dimas7git <dimaslimaferreira@gmail.com>
2023-08-29 10:24:21 -0300

    aula 13 - create connection with database

--- src/main/java/academy/devdojo/springboot2/curso/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/curso/domain/Anime.java
@@ -1,13 +1,21 @@
 // Classe que representa os dados armazenados no banco de dados para a entidade Anime
 package academy.devdojo.springboot2.curso.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data // Anotação Lombok que gera automaticamente getters, setters, toString, etc.
-@AllArgsConstructor // Anotação Lombok que gera um construtor com todos os campos
+@AllArgsConstructor // Anotação Lombok que gera um construtor com todos os campo
+@NoArgsConstructor
+@Entity
 public class Anime {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Identificador único do anime no banco de dados
     private String name; 
 }
