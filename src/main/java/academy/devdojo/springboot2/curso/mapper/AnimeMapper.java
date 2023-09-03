79a6cc84b868e28886166a4bc8159574f23c0016
dimas7git <dimaslimaferreira@gmail.com>
2023-09-03 19:27:44 -0300

    aula15 - add framework mapstruct

--- src/main/java/academy/devdojo/springboot2/curso/mapper/AnimeMapper.java
+++ src/main/java/academy/devdojo/springboot2/curso/mapper/AnimeMapper.java
@@ -7,9 +7,14 @@ import academy.devdojo.springboot2.curso.domain.Anime;
 import academy.devdojo.springboot2.curso.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.curso.requests.AnimePutRequestBody;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring") // Indica que esta classe é um mapeador gerado pelo MapStruct
 public abstract class AnimeMapper {
+    // Obtém uma instância do mapeador usando o Factory Method de MapStruct
     public static final AnimeMapper INSTANCE = Mappers.getMapper(AnimeMapper.class);
+    
+    // Converte uma requisição AnimePostRequestBody em um objeto Anime
     public abstract Anime toAnime(AnimePostRequestBody animePostRequestBody);
-    public abstract Anime toAnime(AnimePutRequestBody animePostRequestBody);
+    
+    // Converte uma requisição AnimePutRequestBody em um objeto Anime
+    public abstract Anime toAnime(AnimePutRequestBody animePutRequestBody);
 }
--- src/main/java/academy/devdojo/springboot2/curso/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/curso/service/AnimeService.java
@@ -1,7 +1,6 @@
 // Classe que implementa a lógica das regras de negócio relacionadas a animes
 package academy.devdojo.springboot2.curso.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -21,29 +20,29 @@ import lombok.RequiredArgsConstructor;
 public class AnimeService {
     
     private final AnimeRepository animeRepository;
+    
+    // Método para listar todos os animes
     public List<Anime> listAll() {
-        // Retorna a lista de animes pré-definida
         return animeRepository.findAll();
     }
 
-    // Método que busca um anime pelo ID (exemplo de regra de negócio)
+    // Método para buscar um anime pelo ID ou lançar uma exceção se não encontrado
     public Anime findByIdOrThrowBadRequestException(long id) {
-        // Busca o anime na lista por ID e lança uma exceção caso não seja encontrado
         return animeRepository.findById(id)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 
-    // Método para salvar um novo anime na lista
+    // Método para salvar um novo anime a partir da requisição AnimePostRequestBody
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         return animeRepository.save(AnimeMapper.INSTANCE.toAnime(animePostRequestBody));
     }
 
-    // Método para deletar um anime da lista pelo ID
+    // Método para deletar um anime pelo ID
     public void delete(long id) {
         animeRepository.delete(findByIdOrThrowBadRequestException(id));
     }
 
-    // Método para substituir um anime na lista pelo ID
+    // Método para substituir um anime pelo ID a partir da requisição AnimePutRequestBody
     public void replace(AnimePutRequestBody animePutRequestBody) {
         Anime savedAnime = findByIdOrThrowBadRequestException(animePutRequestBody.getId());
         Anime anime = AnimeMapper.INSTANCE.toAnime(animePutRequestBody);
